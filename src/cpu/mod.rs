8cb174434d9fc726230f6a7b3ae4c7a733a26a91
Axel Örn Sigurðsson <axel@absalon.is>
2018-08-12 18:19:03 +0900

    Implement LDA

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -36,72 +36,30 @@ impl CPU {
         CPU::default()
     }
 
-    pub fn read_byte(&mut self, addressing: Addressing) -> u8 {
-        match addressing {
-            Addressing::Absolute => {
-                let lsb = self.memory.read(self.pc);
-                self.pc += 1;
+    pub fn read_next_byte(&mut self) -> u8 {
+        let byte = self.memory.read(self.pc);
+        self.pc += 1;
+        byte
+    }
 
-                let msb = self.memory.read(self.pc);
-                self.pc += 1;
+    pub fn read_next_double(&mut self) -> u16 {
+        let lsb = self.read_next_byte();
+        let msb = self.read_next_byte();
+        (u16::from(msb) << 8) + u16::from(lsb)
+    }
 
-                let address = (u16::from(msb) << 8) + u16::from(lsb);
-                self.memory.read(address)
-            }
-            Addressing::AbsoluteX => {
-                let lsb = self.memory.read(self.pc);
-                self.pc += 1;
+    /// Read a byte from an address
+    pub fn read_byte(&self, address: u16) -> u8 {
+        self.memory.read(address)
+    }
 
-                let msb = self.memory.read(self.pc);
-                self.pc += 1;
-
-                let mut address = (u16::from(msb) << 8) + u16::from(lsb) + u16::from(self.x);
-
-                self.memory.read(address)
-            }
-            Addressing::AbsoluteY => {
-                let lsb = self.memory.read(self.pc);
-                self.pc += 1;
-
-                let msb = self.memory.read(self.pc);
-                self.pc += 1;
-
-                let mut address = (u16::from(msb) << 8) + u16::from(lsb) + u16::from(self.y);
-
-                self.memory.read(address)
-            }
-            Addressing::Immediate => {
-                let byte = self.memory.read(self.pc);
-                self.pc += 1;
-                byte
-            }
-            Addressing::Indirect => 0,
-            Addressing::IndirectX => 0,
-            Addressing::IndirectY => 0,
-            Addressing::Relative => {
-                let offset = self.memory.read(self.pc);
-                self.pc += 1;
-                self.pc = self.pc.wrapping_add((offset as i8) as u16);
-                offset
-            }
-            Addressing::ZeroPage => {
-                let address = self.memory.read(self.pc);
-                self.pc += 1;
-                self.memory.read(u16::from(address))
-            }
-            Addressing::ZeroPageX => {
-                let address = self.memory.read(self.pc);
-                self.pc += 1;
-                let address = address.wrapping_add(self.x);
-                self.memory.read(u16::from(address))
-            }
-            Addressing::ZeroPageY => {
-                let address = self.memory.read(self.pc);
-                self.pc += 1;
-                let address = address.wrapping_add(self.y);
-                self.memory.read(u16::from(address))
-            }
-        }
+    /// Read a double from an address
+    ///
+    /// Reads two bytes and combines them in a 16-bit double in little endian
+    pub fn read_double(&self, address: u16) -> u16 {
+        let lsb = self.memory.read(address);
+        let msb = self.memory.read(address + 1);
+        (u16::from(msb) << 8) + u16::from(lsb)
     }
 }
 
@@ -110,134 +68,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn read_byte_absolute() {
+    fn read_next_byte() {
         let mut cpu = CPU {
             pc: 0x0002,
             ..CPU::default()
         };
-        cpu.memory
-            .load_ram(vec![0xFF, 0xFF, 0x05, 0x00, 0xFF, 0xAA]);
+        cpu.memory.load_ram(vec![0xFF, 0xFF, 0xAA, 0xFF]);
 
-        let byte = cpu.read_byte(Addressing::Absolute);
+        let byte = cpu.read_next_byte();
 
         assert_eq!(byte, 0xAA);
-        assert_eq!(cpu.pc, 0x0004);
-    }
-
-    #[test]
-    fn read_byte_absolute_x() {
-        let mut cpu = CPU {
-            pc: 0x0002,
-            x: 1,
-            ..CPU::default()
-        };
-        cpu.memory
-            .load_ram(vec![0xFF, 0xFF, 0x04, 0x00, 0xFF, 0xAA]);
-
-        let byte = cpu.read_byte(Addressing::AbsoluteX);
-
-        assert_eq!(byte, 0xAA);
-        assert_eq!(cpu.pc, 0x0004);
-    }
-
-    #[test]
-    fn read_byte_absolute_y() {
-        let mut cpu = CPU {
-            pc: 0x0002,
-            y: 1,
-            ..CPU::default()
-        };
-        cpu.memory
-            .load_ram(vec![0xFF, 0xFF, 0x04, 0x00, 0xFF, 0xAA]);
-
-        let byte = cpu.read_byte(Addressing::AbsoluteY);
-
-        assert_eq!(byte, 0xAA);
-        assert_eq!(cpu.pc, 0x0004);
-    }
-
-    #[test]
-    fn read_byte_immediate() {
-        let mut cpu = CPU {
-            pc: 0x0001,
-            ..CPU::default()
-        };
-        cpu.memory.load_ram(vec![0xDE, 0xAD, 0xBE, 0xEF]);
-
-        let byte = cpu.read_byte(Addressing::Immediate);
-
-        assert_eq!(byte, 0xAD);
-        assert_eq!(cpu.pc, 0x0002);
-    }
-
-    #[test]
-    fn read_byte_indirect() {
-        let mut cpu = CPU {
-            pc: 0x0001,
-            ..CPU::default()
-        };
-    }
-
-    #[test]
-    fn read_byte_indirect_x() {}
-
-    #[test]
-    fn read_byte_indirect_y() {}
-
-    #[test]
-    fn read_byte_relative() {
-        let mut cpu = CPU {
-            pc: 0x0002,
-            ..CPU::default()
-        };
-        cpu.memory.load_ram(vec![0x00, 0x00, -2i8 as u8, 0x00]);
-
-        cpu.read_byte(Addressing::Relative);
-
-        assert_eq!(cpu.pc, 0x0001);
-    }
-
-    #[test]
-    fn read_byte_zero_page() {
-        let mut cpu = CPU {
-            pc: 0x0001,
-            ..CPU::default()
-        };
-        cpu.memory.load_ram(vec![0xDE, 0x03, 0xBE, 0xEF]);
-
-        let byte = cpu.read_byte(Addressing::ZeroPage);
-
-        assert_eq!(byte, 0xEF);
-        assert_eq!(cpu.pc, 0x0002);
-    }
-
-    #[test]
-    fn read_byte_zero_page_x() {
-        let mut cpu = CPU {
-            pc: 0x0002,
-            x: 0xFF,
-            ..CPU::default()
-        };
-        cpu.memory.load_ram(vec![0xDE, 0xAD, 0x04, 0xEF]);
-
-        let byte = cpu.read_byte(Addressing::ZeroPageX);
-
-        assert_eq!(byte, 0xEF);
-        assert_eq!(cpu.pc, 0x0003);
-    }
-
-    #[test]
-    fn read_byte_zero_page_y() {
-        let mut cpu = CPU {
-            pc: 0x0002,
-            y: 0xFF,
-            ..CPU::default()
-        };
-        cpu.memory.load_ram(vec![0xDE, 0xAD, 0x04, 0xEF]);
-
-        let byte = cpu.read_byte(Addressing::ZeroPageY);
-
-        assert_eq!(byte, 0xEF);
         assert_eq!(cpu.pc, 0x0003);
     }
 }
---
+++ src/cpu/opcodes/storage.rs
@@ -0,0 +1,212 @@
+use cpu::{Addressing, CPU};
+
+/// Load accumulator with memory
+///
+/// # Supported addressing modes
+///
+/// * Absolute - 4 Cycles
+/// * Absolute X - 4* Cycles
+/// * Absolute Y - 4* Cycles
+/// * Indirect X - 6 Cycles
+/// * Indirect Y - 5* Cycles
+/// * Zero Page - 3 Cycles
+/// * Zero Page X - 4 Cycles
+///
+/// \* Add 1 if page boundary is crossed
+pub fn lda(cpu: &mut CPU, addressing: Addressing) -> u8 {
+    let cycles = match addressing {
+        Addressing::Absolute => {
+            let address = cpu.read_next_double();
+            cpu.a = cpu.read_byte(address);
+            4
+        }
+        Addressing::AbsoluteX => {
+            let address = cpu.read_next_double() + u16::from(cpu.x);
+            cpu.a = cpu.read_byte(address);
+            4 // Add 1 if page boundary is crossed
+        }
+        Addressing::AbsoluteY => {
+            let address = cpu.read_next_double() + u16::from(cpu.y);
+            cpu.a = cpu.read_byte(address);
+            4 // Add 1 if page boundary is crossed
+        }
+        Addressing::Immediate => {
+            cpu.a = cpu.read_next_byte();
+            2
+        }
+        Addressing::IndirectX => {
+            let ptr = u16::from(cpu.read_next_byte() + cpu.x);
+
+            let address = cpu.read_double(ptr);
+
+            cpu.a = cpu.read_byte(address);
+            6
+        }
+        Addressing::IndirectY => {
+            let ptr = u16::from(cpu.read_next_byte());
+
+            let address = cpu.read_double(ptr) + u16::from(cpu.y);
+
+            cpu.a = cpu.read_byte(address);
+            5 // Add 1 if page boundary is crossed
+        }
+        Addressing::ZeroPage => {
+            let address = cpu.read_next_byte() as u16;
+            cpu.a = cpu.read_byte(address);
+            3
+        }
+        Addressing::ZeroPageX => {
+            let address = cpu.read_next_byte().wrapping_add(cpu.x) as u16;
+            cpu.a = cpu.read_byte(address);
+            4
+        }
+        _ => panic!("LDA doesn't support {:?} addressing", addressing),
+    };
+
+    cpu.flags.set_zero_from_byte(cpu.a);
+    cpu.flags.set_negative_from_byte(cpu.a);
+    cycles
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn lda_immediate() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            ..CPU::default()
+        };
+        cpu.memory
+            .load_ram(vec![0x01, 0x02, 0x03, 0x04, 0x05, 0x06]);
+
+        let cycles = lda(&mut cpu, Addressing::Immediate);
+
+        assert_eq!(cycles, 2);
+        assert_eq!(cpu.a, 0x03);
+    }
+
+    #[test]
+    fn lda_zeropage() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            ..CPU::default()
+        };
+        cpu.memory.load_ram(vec![0x00, 0xDE, 0x01, 0x00]);
+
+        let cycles = lda(&mut cpu, Addressing::ZeroPage);
+
+        assert_eq!(cycles, 3);
+        assert_eq!(cpu.a, 0xDE);
+    }
+
+    #[test]
+    fn lda_zeropage_x() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            x: 0x02,
+            ..CPU::default()
+        };
+        cpu.memory.load_ram(vec![0x00, 0xDE, 0x01, 0xAD]);
+
+        let cycles = lda(&mut cpu, Addressing::ZeroPageX);
+
+        assert_eq!(cycles, 4);
+        assert_eq!(cpu.a, 0xAD);
+    }
+
+    #[test]
+    fn lda_zeropage_x_wrapping() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            x: 0xFF,
+            ..CPU::default()
+        };
+        cpu.memory.load_ram(vec![0xAA, 0xDE, 0x01, 0xAD]);
+
+        let cycles = lda(&mut cpu, Addressing::ZeroPageX);
+
+        assert_eq!(cycles, 4);
+        assert_eq!(cpu.a, 0xAA);
+    }
+
+    #[test]
+    fn lda_absolute() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            ..CPU::default()
+        };
+        cpu.memory
+            .load_ram(vec![0xFF, 0xFF, 0x05, 0x00, 0xFF, 0xAA]);
+
+        let cycles = lda(&mut cpu, Addressing::Absolute);
+
+        assert_eq!(cycles, 4);
+        assert_eq!(cpu.a, 0xAA);
+    }
+
+    #[test]
+    fn lda_absolute_x() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            x: 0x01,
+            ..CPU::default()
+        };
+        cpu.memory
+            .load_ram(vec![0xFF, 0xFF, 0x05, 0x00, 0xFF, 0xAA, 0xBB]);
+
+        let cycles = lda(&mut cpu, Addressing::AbsoluteX);
+
+        assert_eq!(cycles, 4);
+        assert_eq!(cpu.a, 0xBB);
+    }
+
+    #[test]
+    fn lda_absolute_y() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            y: 0x02,
+            ..CPU::default()
+        };
+        cpu.memory
+            .load_ram(vec![0xFF, 0xFF, 0x05, 0x00, 0xFF, 0xAA, 0xBB, 0xCC]);
+
+        let cycles = lda(&mut cpu, Addressing::AbsoluteY);
+
+        assert_eq!(cycles, 4);
+        assert_eq!(cpu.a, 0xCC);
+    }
+
+    #[test]
+    fn lda_indirect_x() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            x: 0x01,
+            ..CPU::default()
+        };
+        cpu.memory
+            .load_ram(vec![0xFF, 0xAA, 0x04, 0xFF, 0xFF, 0x01, 0x00]);
+
+        let cycles = lda(&mut cpu, Addressing::IndirectX);
+
+        assert_eq!(cycles, 6);
+        assert_eq!(cpu.a, 0xAA);
+    }
+
+    #[test]
+    fn lda_indirect_y() {
+        let mut cpu = CPU {
+            pc: 0x0003,
+            y: 0x01,
+            ..CPU::default()
+        };
+        cpu.memory
+            .load_ram(vec![0xFF, 0xFF, 0xAA, 0x06, 0xFF, 0xFF, 0x01, 0x00]);
+
+        let cycles = lda(&mut cpu, Addressing::IndirectY);
+
+        assert_eq!(cycles, 5);
+        assert_eq!(cpu.a, 0xAA);
+    }
+}
