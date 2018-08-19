3784a428eaf1115a440456dc279ed0b69022212b
Axel Örn Sigurðsson <axel@absalon.is>
2018-08-19 18:30:10 +0900

    Implement INC, INX and INY

---
+++ src/cpu/opcodes/math/increment.rs
@@ -0,0 +1,141 @@
+use cpu::{Addressing, CPU};
+
+/// Increment memory
+///
+/// # Supported addressing modes
+///
+/// * Absolute - 6 Cycles
+/// * Absolute X - 7 Cycles
+/// * Zero Page - 5 Cycles
+/// * Zero Page X - 6 Cycles
+///
+/// # Flags affected
+///
+/// * Zero
+/// * Negative
+pub fn inc(cpu: &mut CPU, addressing: Addressing) -> u8 {
+    let cycles = match addressing {
+        Addressing::Absolute | Addressing::ZeroPageX => 6,
+        Addressing::AbsoluteX => 7,
+        Addressing::ZeroPage => 5,
+        _ => panic!("INC doesn't support {:?} addressing", addressing),
+    };
+
+    let byte = cpu.update_byte(addressing, |x| x.wrapping_add(1));
+
+    cpu.flags.set_zero_from_byte(byte);
+    cpu.flags.set_negative_from_byte(byte);
+
+    cycles
+}
+
+/// Increment X Index
+///
+/// # Flags affected
+///
+/// * Zero
+/// * Negative
+pub fn inx(cpu: &mut CPU) -> u8 {
+    let x = cpu.x.wrapping_add(1);
+    cpu.x = x;
+
+    cpu.flags.set_zero_from_byte(x);
+    cpu.flags.set_negative_from_byte(x);
+
+    2
+}
+
+/// Increment Y Index
+///
+/// # Flags affected
+///
+/// * Zero
+/// * Negative
+pub fn iny(cpu: &mut CPU) -> u8 {
+    let y = cpu.y.wrapping_add(1);
+    cpu.y = y;
+
+    cpu.flags.set_zero_from_byte(y);
+    cpu.flags.set_negative_from_byte(y);
+
+    2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn inc_increments_memory() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            ..CPU::default()
+        };
+        cpu.memory.load_ram(vec![0xFF, 0xA1, 0x01, 0x00]);
+
+        inc(&mut cpu, Addressing::Absolute);
+
+        assert_eq!(cpu.raw_read_byte(0x0001), 0xA2);
+    }
+
+    #[test]
+    fn inc_increments_memory_wrapping() {
+        let mut cpu = CPU {
+            pc: 0x0002,
+            ..CPU::default()
+        };
+        cpu.memory.load_ram(vec![0xFF, 0xFF, 0x01, 0x00]);
+
+        inc(&mut cpu, Addressing::Absolute);
+
+        assert_eq!(cpu.raw_read_byte(0x0001), 0x00);
+    }
+
+    #[test]
+    fn inx_increments_x_index() {
+        let mut cpu = CPU {
+            x: 0xA1,
+            ..CPU::default()
+        };
+
+        inx(&mut cpu);
+
+        assert_eq!(cpu.x, 0xA2);
+    }
+
+    #[test]
+    fn inx_increments_x_index_wrapping() {
+        let mut cpu = CPU {
+            x: 0xFF,
+            ..CPU::default()
+        };
+
+        inx(&mut cpu);
+
+        assert_eq!(cpu.x, 0x00);
+    }
+
+    #[test]
+    fn iny_increments_x_index() {
+        let mut cpu = CPU {
+            y: 0xA1,
+            ..CPU::default()
+        };
+
+        iny(&mut cpu);
+
+        assert_eq!(cpu.y, 0xA2);
+    }
+
+    #[test]
+    fn iny_increments_x_index_wrapping() {
+        let mut cpu = CPU {
+            y: 0xFF,
+            ..CPU::default()
+        };
+
+        iny(&mut cpu);
+
+        assert_eq!(cpu.y, 0x00);
+    }
+}
