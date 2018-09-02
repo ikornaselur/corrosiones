6e475696387bfdfad832ee1a3170c91783d3a4f0
Axel Örn Sigurðsson <axel@absalon.is>
2018-09-02 17:47:54 +0900

    Run 02-implied tests

--- tests/instr_test_v5/mod.rs
+++ tests/instr_test_v5/mod.rs
@@ -6,11 +6,9 @@ extern crate corrosiones;
 use instr_test_v5::corrosiones::cpu::CPU;
 use instr_test_v5::corrosiones::utils::read_blargg_message;
 
-#[test]
-fn basics() {
+fn run_blargg_test(path: &str) {
     let mut cpu = CPU::new();
-    cpu.load_file(String::from("tests/instr_test_v5/01-basics.nes"))
-        .unwrap();
+    cpu.load_file(String::from(path)).unwrap();
 
     loop {
         if [0xDE, 0xB0, 0x61]
@@ -32,4 +30,14 @@ fn basics() {
         }
         cpu.step(false);
     }
+}
+
+#[test]
+fn basics() {
+    run_blargg_test("tests/instr_test_v5/01-basics.nes");
+}
+
+#[test]
+fn implied() {
+    run_blargg_test("tests/instr_test_v5/02-implied.nes");
 }
